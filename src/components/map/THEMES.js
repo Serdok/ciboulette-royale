2cff42706f5a1a599612e0349c5800646b41c7d1
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2021-09-28 17:11:56 +0200

    Refactored web map

---
+++ src/components/map/THEMES.js
@@ -0,0 +1,38 @@
+export const THEMES = [
+    {
+        name: 'Boutiques & Shopping',
+        icon: require('./popup-icons/shop.png'),
+    },
+    {
+        name: 'Activités, Loisirs et Bien-être',
+        icon: require('./popup-icons/activity.png'),
+    },
+    {
+        name: 'Restaurants & Gastronomie',
+        icon: require('./popup-icons/restaurant.png'),
+    },
+    {
+        name: 'Lyon Pratique',
+        icon: require('./popup-icons/location.png'),
+    },
+    {
+        name: 'Lieux de spectacles',
+        icon: require('./popup-icons/show.png'),
+    },
+    {
+        name: 'Culture & Musées',
+        icon: require('./popup-icons/museum.png'),
+    },
+    {
+        name: 'Patrimoine - Unesco',
+        icon: require('./popup-icons/statue.png'),
+    },
+    {
+        name: 'Nocturne',
+        icon: require('./popup-icons/drinks.png'),
+    },
+    {
+        name: 'Hébergements',
+        icon: require('./popup-icons/hotel.png'),
+    },
+];
--- src/components/map/popup.js
+++ src/components/map/popup.js
@@ -1,20 +1,21 @@
-const popupContent = {
-    textAlign: "center",
-    height: "350px",
-    marginTop: "30px"
-  };
-  const popupHead = {
-    fontWeight: "bold",
-    fontSize: "22px"
-  };
-  
-  const popupText = {
-    fontSize: "15px",
-    marginBottom: "20px"
-  };
-  
-  const okText = {
-    fontSize: "15px"
-  };
-  
-  export { popupContent, popupHead, popupText, okText };
+import React from 'react';
+import { Popup } from 'react-leaflet';
+
+export default function MapPopup({ image, name, address, contact }) {
+  return (
+      <>
+        <Popup>
+          <img alt={'image'} src={image} width={150} height={150}/>
+          <div style={{'text-align': 'center', 'align-content': 'center'}}>
+            <a target={'_blank'} href={contact?.['Site web (URL)']}>{ name }</a>
+            <br/>
+            <p style={{'margin': 0}}>Adresse</p>
+            <p style={{'margin': 0}}>{ address?.streetAddress }</p>
+            <p style={{'margin': 0}}>{ address?.postalCode }</p>
+            <p style={{'margin': 0}}>Contact</p>
+            <p style={{'margin': 0}}>{ contact?.['Téléphone'] }</p>
+          </div>
+        </Popup>
+      </>
+  );
+}
